575d6bf1b23058587cf46e0c3d7c51f9afe6079b
Haydn Hadley <codingmonkey80@gmail.com>
2018-01-03 17:41:38 +0000

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,5 @@
 (function (window) {
-// NOTE! The steps in this file are basically identical to the ones you
+// NOTE! The steps in this file are basically identicalto the ones you
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
